572640b59b5e9be9852cfdd39e697d339c2fdc42
Davide <davidesut@gmail.com>
2019-10-07 19:31:30 +0200

    Aggiunti i file della classe

---
+++ Classe ORARIO/Classe_ORARIO.cpp
@@ -0,0 +1,2 @@
+#include<iostream>
+#include<orario.h>
---
+++ Classe ORARIO/orario.h
@@ -0,0 +1,9 @@
+class orario{
+    private:
+    int sec;
+    public:
+    orario(int=0,int=0,int=0);
+    int Ore();
+    int Minuti();
+    int Secondi();
+};
